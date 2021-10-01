5b49541cdb417f682f766923d2997ce434033226
Durgesh Singh <durgesh.singh@vispl.net.in>
2021-10-01 14:39:57 +0530

    dweq

---
+++ WebApplication33/Controllers/D2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication33.Controllers
+{
+    public class D2Controller : Controller
+    {
+        // GET: D2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
